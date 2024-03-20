da3ac1acf1d9062e54bdf2284dee2f998d1dfee7
momyschild <163287742+momyschild@users.noreply.github.com>
2024-03-20 10:52:05 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,44 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # . . . #
+        # # . # #
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # . . . #
+        # # . # #
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        # . . . #
+        # # . # #
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # . . . #
+        # # . # #
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+})
+basic.showLeds(`
+    # . . . #
+    # # . # #
+    . . . . .
+    . # # # .
+    . . . . .
+    `)
+basic.forever(function () {
+	
+})
